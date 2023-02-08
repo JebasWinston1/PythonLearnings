74ba88ef22585a0702db1e09b534dcc06b710dd0
Jebas Winston E <jebaswinston55@gmail.com>
2023-02-08 14:53:05 +0530

    String Formatting

---
+++ main.py
@@ -0,0 +1,5 @@
+ ##* String Formatting
+print('This is a {}'.format('cat'))
+print('This {} {} {}'.format('is', 'a', 'cat'))
+print('This {2} {2} {0}'.format('is', 'a', 'cat'))
+print('This {c} {a} {i}'.format(i='is', a='a', c='cat'))
