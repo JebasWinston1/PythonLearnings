5be59ca27e2626887a68f23cec42f739ba3d30c8
Jebas Winston E <jebaswinston55@gmail.com>
2023-02-08 15:04:09 +0530

    Adding Formatted String Literals

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print('This is a {}'.format('cat'))
 print('This {} {} {}'.format('is', 'a', 'cat'))
 print('This {2} {2} {0}'.format('is', 'a', 'cat'))
 print('This {c} {a} {i}'.format(i='is', a='a', c='cat'))
+
+##* F String or Formatted String Literals
+name = 'Jose'
+age = 21
+print(f'{name} is {age} years old')
